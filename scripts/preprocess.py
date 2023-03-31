914c40d6ed515a97a44cf9849787e4409ba7ff06
cmoore <cmoore24@ncsu.edu>
2023-03-31 19:09:36 -0400

    descriptive IDs

--- scripts/preprocess.py
+++ scripts/preprocess.py
@@ -11,11 +11,16 @@ def generate_unique_id(row, id_dict):
     return new_id
 
 #----- Main ------------------------------------------------------------------------------------------------------------------------
-data = pd.read_csv("data/raw_prelim_careless_data.csv", dtype={'ID': str})
+data = pd.read_csv("data/raw_prelim_careless_data.csv")
+rater1_data = pd.read_csv("data/rater1_code.csv", dtype={'ID': str})
 
 # Add ID column
 id_dict = {}
 data['ID'] = data.apply(lambda row: generate_unique_id(row, id_dict), axis=1)
 
+id_dict = {}
+rater1_data['ID'] = rater1_data.apply(lambda row: generate_unique_id(row, id_dict), axis=1)
+
 # Export
 data.to_csv("data/prelim_careless_data.csv", index=False)
+rater1_data.to_csv("data/rater1_code.csv", index=False)
