a930c145e9d47a61087663713cc8d00a105191b0
cmoore <cmoore24@ncsu.edu>
2023-04-02 12:14:41 -0400

    update plot

--- scripts/forest_plot.py
+++ scripts/forest_plot.py
@@ -1,6 +1,7 @@
 
 import pandas as pd
 import numpy as np
+from scipy.stats import norm
 import matplotlib
 import matplotlib.pyplot as plt
 matplotlib.rcParams.update({'text.usetex': True})
@@ -10,30 +11,31 @@ from scripts.proportions_pooled import get_pooled_statistics
 
 #----- Functions ------------------------------------------------------------------------------------------------------------------
 def normalize_weights(weights, min_size=3, max_size=7):
+    """Normalize weights to a given range."""
     norm_weights = (weights - min(weights)) / (max(weights) - min(weights))
     return min_size + norm_weights * (max_size - min_size)
 
-def create_forest_plot(data):
+def create_forest_plot(data, pooled_p, lower_ci, upper_ci):
+    # Reverse the data
+    data = data.iloc[::-1]
     fig, ax = plt.subplots(figsize=(10, len(data) * 0.2))  # Add extra space for the pooled results
-    # plot title
-    ax.set_title(r'\textbf{Forest Plot}', fontsize=16)
+    #ax.set_title(r'\textbf{Forest Plot}', fontsize=16)
     # y axis
     for i, row in data.iterrows():
-        ax.errorbar(row['proportions_total'], i + 1, xerr=(row['ci_upper'] - row['ci_lower']) / 2, fmt='o', markersize=row['normalized_weight'], capsize=2, elinewidth=1.25, ecolor='black', color='black')
+        ax.errorbar(row['proportions_total'], len(data) - i, xerr=(row['ci_upper'] - row['ci_lower']) / 2, fmt='o', markersize=row['normalized_weight'], capsize=2, elinewidth=1.25, ecolor='black', color='black')
     ax.axvline(0, color='gray', linestyle='--', lw=0.5)
-    ax.set_yticks([0] + list(data.index + 1))  # Add an extra tick for the pooled results
+    ax.set_yticks(list(range(len(data) + 1)))  # Add an extra tick for the pooled results
     ax.set_yticklabels(['Pooled Prevalence'] + data['ID'].tolist())
     ax.text(-0.067, len(data) + 1.5, r'\underline{\textbf{Study}}', fontsize=10, verticalalignment='top')
-    ax.yaxis.grid(True, linestyle='--', alpha=0.5, which='both') # Add grid to y-axis    
+    ax.yaxis.grid(True, linestyle='--', alpha=0.5, which='both') # Add grid to y-axis
     # Add the pooled results
-    pooled_p, _, lower_ci, upper_ci, _ = get_pooled_statistics(p, n)
     ax.errorbar(pooled_p, 0, xerr=(upper_ci - lower_ci) / 2, fmt='o', markersize=7, capsize=2, elinewidth=1.25, ecolor='red', color='red')
     ax.axvline(pooled_p, color='red', linestyle='--', lw=0.5)  # Add a vertical dashed line through the red circle
     # y axis 2
     ax2 = ax.twinx()
-    ax2.set_yticks([0] + list(data.index + 1))
-    yticklabels = [''] + (data['proportions_total'].round(2).astype(str) + ' (' + data['ci_lower'].round(2).astype(str) + '-' + data['ci_upper'].round(2).astype(str) + ')').tolist()
-    yticklabels[0] = str(round(pooled_p, 2)) + ' (' + str(round(lower_ci, 2)) + '-' + str(round(upper_ci, 2)) + ')'
+    ax2.set_yticks(list(range(len(data) + 1)))  # Add an extra tick for the pooled results
+    yticklabels = [] + (data['proportions_total'].round(3).astype(str) + ' (' + data['ci_lower'].round(3).astype(str) + '-' + data['ci_upper'].round(3).astype(str) + ')').tolist()
+    yticklabels.insert(0, str(round(pooled_p, 3)) + ' (' + str(round(lower_ci, 3)) + '-' + str(round(upper_ci, 3)) + ')')
     ax2.set_yticklabels(yticklabels)
     ax2.yaxis.tick_right()
     ax2.set_ylim(ax.get_ylim())
@@ -52,14 +54,17 @@ def create_forest_plot(data):
 data = pd.read_excel('results/raw_proportions.xlsx', sheet_name='proportions_total')
 data['weight'] = 1 / (data['proportions_total'] * (1 - data['proportions_total']) / data['sample_size'])
 data['normalized_weight'] = normalize_weights(data['weight'])
-data['marker_size'] = np.sqrt(data['weight'])
 p = data['proportions_total']
 n = data['sample_size']
+se = data['se']
+var = se**2
+
+# get pooled statistics
+pooled_p, _, lower_ci, upper_ci, _ = get_pooled_statistics(p, n)
 
 # create forest plot
-plot = create_forest_plot(data)
+plot = create_forest_plot(data, pooled_p, lower_ci, upper_ci)
 
 # save plot
 plot.savefig('results/forest_plot.png', dpi=300, bbox_inches='tight')
-
 
